8b65bfb78e80a8b7c91efba87b372d719919c0a4
Mat Broughton <mat@broughty.com>
2014-01-08 20:03:18 +0000

    League goals measured

--- btts-war/src/main/webapp/app/controllers/choices-controller.js
+++ btts-war/src/main/webapp/app/controllers/choices-controller.js
@@ -4,12 +4,24 @@ bttsApp.controller('CurrentChoicesController', function ($scope, $http, $log) {
         success(function (data) {
             $scope.currentChoices = data;
         }).
-        error(function(data, status, headers, config) {
+        error(function (data, status, headers, config) {
             $log.log("Failed because status " + status + " and with data " + data);
             $log.log("Failed because headers " + headers + " and with config " + config);
-            $scope.currentChoices = [{"dateEntered":null,"week":"unknown","player":"Error","choice1":"","choice2":"","choice3":"","choice4":"","choice1Result":"WAITING","choice2Result":"FAIL","choice3Result":"FAIL","choice4Result":"FAIL","choice1Points":0,"choice2Points":0,"choice3Points":0,"choice4Points":0,"alerted":false,"defaultChoices":false}];
+            $scope.currentChoices = [
+                {"dateEntered":null, "week":"unknown", "player":"Error", "choice1":"", "choice2":"", "choice3":"", "choice4":"", "choice1Result":"WAITING", "choice2Result":"FAIL", "choice3Result":"FAIL", "choice4Result":"FAIL", "choice1Points":0, "choice2Points":0, "choice3Points":0, "choice4Points":0, "alerted":false, "defaultChoices":false}
+            ];
             $scope.status = status;
         });
+
+    $scope.awesome = function (choice) {
+        if (choice === 'WAITING'.valueOf()) {
+            return 'fa fa-question-circle'.valueOf();
+        }
+        if (choice === 'FAIL'.valueOf()) {
+            return 'fa fa-thumbs-down'.valueOf();
+        }
+        return 'fa fa-thumbs-up'.valueOf();
+    }
 });
 bttsApp.controller('CurrentStarChoicesController', function ($scope, $http, $log) {
     $scope.$log = $log;
@@ -17,10 +29,12 @@ bttsApp.controller('CurrentStarChoicesController', function ($scope, $http, $log
         success(function (data) {
             $scope.currentChoices = data;
         }).
-        error(function(data, status, headers, config) {
+        error(function (data, status, headers, config) {
             $log.log("Failed because status " + status + " and with data " + data);
             $log.log("Failed because headers " + headers + " and with config " + config);
-            $scope.currentChoices = [{"dateEntered":null,"week":"unknown","player":"Error","choice1":"","choice2":"","choice3":"","choice4":"","choice1Result":"WAITING","choice2Result":"FAIL","choice3Result":"FAIL","choice4Result":"FAIL","choice1Points":0,"choice2Points":0,"choice3Points":0,"choice4Points":0,"alerted":false,"defaultChoices":false}];
+            $scope.currentChoices = [
+                {"dateEntered":null, "week":"unknown", "player":"Error", "choice1":"", "choice2":"", "choice3":"", "choice4":"", "choice1Result":"WAITING", "choice2Result":"FAIL", "choice3Result":"FAIL", "choice4Result":"FAIL", "choice1Points":0, "choice2Points":0, "choice3Points":0, "choice4Points":0, "alerted":false, "defaultChoices":false}
+            ];
             $scope.status = status;
         });
 });
